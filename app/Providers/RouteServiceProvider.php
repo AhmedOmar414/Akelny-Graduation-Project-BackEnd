b128131880aebe6cf62dff0c3e9c1df894fa7d96
unknown <ahmmedd606@gmail.com>
2022-10-28 22:41:58 +0200

    LoginAndRegisterSetupViews

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            //user controllers
+            Route::middleware('web')->namespace('App\Http\Controllers\Website\Auth\User')
+                ->group(base_path('routes/user/user-auth-api.php'));
         });
     }
 
---
+++ routes/user/user-auth-api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Website\Auth\User\LoginController;
+use App\Http\Controllers\Website\Auth\User\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix'=>'user/'],function (){
+    Route::get('auth-page',[RegisterController::class,'returnAuthPage']);
+});
