9e87834df378df40bb852baeb9cea2eeb78fa132
shady <smetwally@visions-tech.net>
2023-02-20 18:42:27 +0200

    dashboard

---
+++ resources/views/dashboard/layout/scripts.blade.php
@@ -0,0 +1,19 @@
+<script src="{{asset('assets/dashboard/assets/vendor/libs/jquery/jquery.js')}}"></script>
+<script src="{{asset('assets/dashboard/assets/vendor/libs/popper/popper.js')}}"></script>
+<script src="{{asset('assets/dashboard/assets/vendor/js/bootstrap.js')}}"></script>
+<script src="{{asset('assets/dashboard/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+
+<script src="{{asset('assets/dashboard/assets/vendor/js/menu.js')}}"></script>
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="{{asset('assets/dashboard/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+
+<!-- Main JS -->
+<script src="{{asset('assets/dashboard/assets/js/main.js')}}"></script>
+
+<!-- Page JS -->
+<script src="{{asset('assets/dashboard/assets/js/dashboards-analytics.js')}}"></script>
+
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
